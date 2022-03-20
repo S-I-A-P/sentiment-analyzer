53c1d2516d1b500ff18058de7adf658a63a1a475
Milica Siriski <milica.siriski998@gmail.com>
2022-03-20 17:45:09 +0100

    SVM training and testing, error calculation

--- svm_users/user_tweet_data.py
+++ svm_users/user_tweet_data.py
@@ -7,3 +7,14 @@ class UserTweetData:
     def __str__(self) -> str:
         return 'total: %d, positive: %d, negative: %d, neutral: %d' % (
             self.total, self.positive, self.negative, self.neutral)
+
+    def calculate_value(self) -> float:
+        coef_positive = 1
+        coef_negative = -1
+        coef_neutral = 0
+
+        if self.total == 0:
+            return 0.0
+
+        sentiment_sum = coef_positive * self.positive + coef_negative * self.negative + coef_neutral * self.neutral
+        return sentiment_sum / self.total
